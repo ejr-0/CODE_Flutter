7b3d2840b55c3d82d2da1468dc9f34dba422717f
Emmanuel Tadena Jr <tadena.ejr@gmail.com>
2024-01-06 14:40:57 +0800

    Flutter

--- 04-Flutter_Hive/02/app/lib/main.dart
+++ 04-Flutter_Hive/02/app/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openBox('SampleBox');
+
   runApp(const MyApp());
 }
 
@@ -10,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -20,7 +27,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -29,8 +35,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _sampleBox = Hive.box('SampleBox');
+
+  ///[createData] function
+  Future<void> createData(Map data) async {
+    await _sampleBox.add(data);
+    // ignore: avoid_print
+    print('${_sampleBox.keys}, ${_sampleBox.values.indexed}');
+  }
+
+  ///[updateData] function
+  Future<void> updateData(Map data, int key) async {
+    await _sampleBox.put(key, data);
+    // ignore: avoid_print
+    print('${_sampleBox.keys}, ${_sampleBox.values.indexed}');
+  }
+
+  ///[deleteData] function
+  Future<void> deleteData(int key) async {
+    await _sampleBox.delete(key);
+    // ignore: avoid_print
+    print('${_sampleBox.keys}, ${_sampleBox.values.indexed}');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hive'),
+        backgroundColor: Colors.black12,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: OutlinedButton(
+                  onPressed: () {
+                    createData({
+                      'product': 'apple',
+                      'quantity': 23,
+                    });
+                  },
+                  child: const Text('Create Data'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: OutlinedButton(
+                  onPressed: () {
+                    updateData(
+                      {
+                        'product': 'apple',
+                        'quantity': 100,
+                      },
+                      1,
+                    );
+                  },
+                  child: const Text('Update Data'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: OutlinedButton(
+                  onPressed: () {
+                    deleteData(14);
+                  },
+                  child: const Text('Delete Data'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: OutlinedButton(
+                  onPressed: () {
+                    // ignore: avoid_print
+                    print(_sampleBox.keys);
+                    // ignore: avoid_print
+                    print(_sampleBox.values);
+                  },
+                  child: const Text('Print Data'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
